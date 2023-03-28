bf2f35882d7f25602b1f5e4ec46dd996659b5916
Informatica <llara@mail.upla.edu.pe>
2023-03-28 10:35:36 -0500

    Creación del servidor API REST

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "io/ioutil"
+	// "log"
+	"net/http"
+	// "strings"
+	"time"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	time.LoadLocation("America/Lima")
+	godotenv.Load();
+
     fmt.Println("Hello, World!")
+
+	var go_port string = os.Getenv("GO_PORT");
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "API GO LANG",
+		})
+	})
+
+	r.Run(go_port)
 }
